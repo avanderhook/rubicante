3c803bcc342063d0e68b422d044192eba8e5ca3b
Adam VanderHook <avanderhook@gmail.com>
2009-03-27 16:25:37 -0400

    Removed website stuff into its own class

--- lib/rubicante/host.rb
+++ lib/rubicante/host.rb
@@ -1,4 +1,4 @@
-require 'net/http'
+require 'rubicante/website'
 
 module Rubicante
   class Host
@@ -29,59 +29,19 @@ module Rubicante
     end
 
     def website(website_url)
-      @websites << website_url
+      @websites << Website.new(website_url)
       self
     end
 
-    # Performs and HTTP GET on the URL's root ('/') and determines
-    # whether or not the page is functioning properly.
-    #
-    # == Is The Site Functioning?
-    #
-    # When performing an HTTP GET on the root, if a 2xx (HTTP OK) or
-    # 3xx (HTTP Redirect) response is received, the method considers the
-    # site to be workinging nominally.
-    #
-    # However, if a 4xx (HTTP Client Error) or 5xx (HTTP Server Error)
-    # response is received, the method considers the site to be working
-    # abnormally.
-    #
-    # == Returns: a Hash
-    #
-    #   {
-    #     :is_up => boolean, # whether or not the page is ok (see above)
-    #     :code  => String   # the HTTP status code (i.e., "200", "301", "401", "500", etc.)
-    #   }
-    #
-    # == Example
-    #
-    #   host = Rubicante::Host.new("test-host")
-    #   response = host.check_website('www.example.com')
-    #
-    #   puts response[:is_up]
-    #   puts response[:code]
-    def check_website(website_url)
-      result = {:is_up  => false,
-                :code   => nil,
-                :url    => website_url
-      }
-      
-      result[:code]   = Net::HTTP.get_response(website_url, '/').code
-      result[:is_up]  = true if result[:code].match(/^(2|3)/)
-
-      return result
-    end
-
-    # Iterates through all the websites in the object and runs
-    # them against ((<check_website>)), appending each websites'
-    # URL to the results before ((*yield*))ing them.
+    # Iterates through all the websites in the Host and runs
+    # is_ok? against them looking for problems.  If a problem is
+    # found, a Hash of the URL and the HTTP Status code is yielded.
     #
     # == Yields: a Hash
     #
     #   {
-    #     :is_up  => boolean, # see ((<check_website>)) for more information
-    #     :code   => String,  # see ((<check_website>)) for more information
-    #     :url    => String   # the URL of the current website in the block
+    #     :url    => String,  # the URL of the current website in the block
+    #     :code   => String   # the HTTP Status code of the check (i.e., 404, 500, etc.)
     #   }
     #
     # == Example
@@ -91,21 +51,15 @@ module Rubicante
     #   host.website('www.rubicante.com')
     #   host.website('www.openbsd.org')
     #
-    #   host.check_websites |response|
-    #     puts "Checking site '#{response[:url]}'..."
-    #     puts "Site returned HTTP Code #{response[:code]}"
-    #
-    #     if response[:is_up]
-    #       puts "Site is OK"
-    #     else
-    #       puts "Site is NOT WORKING!"
-    #     end
+    #   host.check_websites |result|
+    #     puts "Website #{result[:url]} failed with code #{result[:code]}!"
     #   end
     def check_websites
-      @websites.each do |url|
-        result = check_website(url)
-        result[:url] = url
-        yield result
+      @websites.each do |website|
+        if not website.is_ok?
+          result = { :url => website.url, :code => website.response_code }
+          yield result
+        end
       end
     end
   end
--- spec/host_spec.rb
+++ spec/host_spec.rb
@@ -6,12 +6,7 @@ describe "A newtork host" do
     @host_name = "spec-host"
     @host = Rubicante::Host.new(@host_name)
 
-    @test_url = "http://www.rubicante-test.com/"
-
-    @http_ok           = Net::HTTPOK.new('1', '200', 'OK')
-    @http_redirect     = Net::HTTPMovedPermanently.new('1', '301', 'Moved Permanently')
-    @http_client_error = Net::HTTPNotFound.new('1', '404', 'File Not Found')
-    @http_server_error = Net::HTTPInternalServerError.new('1', '500', 'Internal Server Error')
+    Net::HTTP.stub!(:get_response).and_return(Net::HTTPServerError.new('1', '500', 'Internal Server Error'))
   end
 
   it "should exist" do
@@ -72,41 +67,11 @@ describe "A newtork host" do
   it "should allow adding more webistes with site()" do
     url = "http://www.example.com/"
     @host.website(url)
-    @host.websites.include?(url).should == true
+    @host.websites.size.should == 1
   end
 
   it "should return self for site()" do
     @host.website("http://www.example.com/").should be_an_instance_of(Rubicante::Host)
-  end
-
-  def check_website
-    result = @host.check_website(@test_url)
-    result[:is_up]
-  end
-
-  it "should return true for good websites" do
-    Net::HTTP.stub!(:get_response).and_return(@http_ok)
-    check_website.should == true
-  end
-
-  it "should return true for redirected websites" do
-    Net::HTTP.stub!(:get_response).and_return(@http_redirect)
-    check_website.should == true
-  end
-
-  it "should return false for websites having problems" do
-    Net::HTTP.stub!(:get_response).and_return(@http_server_error)
-    check_website.should == false
-  end
-
-  it "should return false for missing files" do
-    Net::HTTP.stub!(:get_response).and_return(@http_client_error)
-    check_website.should == false
-  end
-
-  it "should return the HTTP code number for a site" do
-    Net::HTTP.stub!(:get_response).and_return(@http_redirect)
-    @host.check_website(@test_url)[:code].should == "301"
   end
 
   it "should check all websites" do
@@ -121,8 +86,6 @@ describe "A newtork host" do
     sites.each do |site|
       my_host.website(site)
     end
-
-    Net::HTTP.stub!(:get_response).and_return(@http_ok)
 
     my_host.check_websites do |result|
       sites.include?(result[:url]).should == true
