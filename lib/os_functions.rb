e1042ab772a556cf51cfcb44c06c02a4be285c82
avanderhook <avanderhook@WDC00002181.TRI.NET>
2009-12-23 13:33:36 -0500

    Treat Cygwin as a Windows platform

--- lib/os_functions.rb
+++ lib/os_functions.rb
@@ -1,5 +1,5 @@
 module OsFunctions
   def is_windows?
-    RUBY_PLATFORM.downcase.include?("mswin")
+    RUBY_PLATFORM.downcase.include?("mswin") or RUBY_PLATFORM.downcase.include?("cygwin")
   end
 end
